c6d393f5d28ee10880652c2c5ca6d239d1547a21
calher99 <calher99@gmail.com>
2022-12-12 10:12:56 +0000

    create central function process weather to manage the extraction of the data and the rendering

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-// 1st geo location API
-// https://openweathermap.org/api/geocoding-api
-// 2nd 5 days forecast
-// https://openweathermap.org/forecast5
-
 
 async function getCoordinates(city) {
     try{
@@ -12,19 +7,46 @@ async function getCoordinates(city) {
         const latitude = data[0].lat;
         const longitude = data[0].lon;
         getWeather(await latitude, await longitude);
-
     }catch (error){
         console.log(error);
     }
 }
 
 async function getWeather (lat, lon){
-    console.log(lat)
-    console.log(lon)
+    try{
+        const link = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=44e48fc145b6aa381c040fcc939ec2e5`
+        const response = await fetch (link);
+        const data = await response.json();
+        processWeather(await data);
+    }catch (error){
+        console.log(error);
+    }
 }
 
-getCoordinates('madrid');
+function processWeather (data){
+    // Function to extract the data we want?¿ or print
+    // 3 days 3 times per day
+    // 00:00 9:00 and 15:00
+    // temp, clouds and rain?¿
+    // Get day today and hour and loop through array?¿
+    // Every 3 hours --> 00 3 6 9 12 15 18 21 00
+    // console.log(data.list[0]);
 
+    //---Extract weather
+    //takes 1 position of the array and returns an object with the key parameters to print
+    extractWeather (data.list[0])
+    //Printweather
+    //Gets the object and renders it in the screen
+    const sky = data.list[0].weather[0].main;
+    const sky_description = data.list[0].weather[0].description;
+    const temp=data.list[0].main.temp;
+}
+
+getCoordinates('barcelona')
+
+function extractWeather(obj) {
+    
+}
 
 
 // NOT WORKING
